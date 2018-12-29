3708cd1c0a6e91678f7df436086ba4264b68c02b
Stuart Hinson <stuart.hinson@gmail.com>
2018-12-29 15:37:31 -0800

    second monitor config

---
+++ .screenlayout/second-monitor.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+xrandr | grep 'HDMI1 connected'
+xrandr --output HDMI-2 --off --output HDMI-1 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1 --off --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off
